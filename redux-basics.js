530be0daf4a40e28b3f2ece665164891c510e5b6
mstfakdgn <mustafa/akiler@gmail.com>
2020-10-06 14:30:37 +0300

    redux with node

--- redux-basics.js
+++ redux-basics.js
@@ -2,17 +2,37 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-    counter:0
-}   
+    counter: 0
+}
 
 //reducer
 const rootReducer = (state = initialState, action) => {
-    return state;
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+
 }
 // store
 const store = createStore(rootReducer);
-console.log(store.getState());
-
-// dispatching action
+//console.log(store.getState());
 
 //subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+// dispatching action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+// console.log(store.getState());
+
